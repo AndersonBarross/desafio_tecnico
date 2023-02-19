7a2f7819b22dee639b1a90beb94eace426dfa43c
Anderson Barros <andersonbarros.rg@gmail.com>
2023-02-19 01:18:07 -0300

    feature adjustment

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
-
-import React, { useState, useContext } from "react";
-//import { useFonts } from "expo-font";
-import { KeyboardAvoidingView, StatusBar } from 'react-native';
+import React from "react";
+import { StatusBar } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { Welcome } from "./src/components/Welcome";
 import { SignIn } from "./src/components/SignIn";
 import { SignUp } from "./src/components/SignUp";
 import { List } from "./src/components/List/index";
-//import { Registration } from "./src/components/Registration";
-//import { AuthProvider } from "./src/Registration/Contexts/Auth";
-//import { SignUp1 } from "./src/Registration/SignUp";
 
 
 const Stack = createNativeStackNavigator();
@@ -42,38 +37,3 @@ function App() {
 };
 
 export default App;
-
-// -------------------------------------------------
-
-
-
-
-//============================================================
-/*
-// original
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Welcome } from './src/components/Welcome';
-
-export default function App() {
-  return (
-    <>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-
-    </>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-*/
